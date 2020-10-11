1261f31075bdebb2d60484755402c853cd56eab0
Chris Y <christopherbcyeh@gmail.com>
2020-10-11 15:42:44 -0700

    Calculation for text length and rating relationship

--- PA1/regression.py
+++ PA1/regression.py
@@ -17,9 +17,27 @@ data_list = list(_parse_data('data/fantasy_10000.json'))
 print("Done")
 data = np.array(data_list)
 
-# Train a predictor to estimate rating from review length
-for review in data:
-    text = review['review_text']
 
-print(data[0])
-print(len(data))
+def _text_length_feature(datum):
+    """Return a vector of just one feature,
+    The length of the review text in characters
+    """
+    feat = [1, len(datum['review_text'])]
+    return feat
+
+
+# Train a predictor to estimate rating from review length
+x = [_text_length_feature(d) for d in data]  # Vector of 1 and length
+y = [d['rating'] for d in data]  # Ratings
+theta, residuals, rank, s = np.linalg.lstsq(x, y)  # Solve matrix equation
+
+theta  # [3.68568136e+00   6.87371675e-05] Positive, longer reviews lead to higher ratings?
+
+
+def _text_length_linear_reg(theta, review_length):
+    """Represents the equation rating = θ_0 + θ_1 * length of review
+    """
+    return theta[0] + theta[1] * review_length
+
+
+# Calculate MSE
